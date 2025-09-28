9b4ca70bb77b40bb256ef5caa598c48745f1e965
Jana-Ruth <ruthewenode73@gmail.com>
2025-09-28 07:57:34 +0100

    update

--- src/common/index.js
+++ src/common/index.js
@@ -4,170 +4,170 @@ const backendDomain = process.env.REACT_APP_BACKEND_URL
 
 const SummaryApi = {
     SignUp : {
-        url : `https://jana-s-backend-faty.vercel.app//api/signup`,
+        url : `https://jana-s-backend-faty.vercel.app/api/signup`,
         method : "post"
     },
     SignIn : {
-        url : `https://jana-s-backend-faty.vercel.app//api/signin`,
+        url : `https://jana-s-backend-faty.vercel.app/api/signin`,
         method : "post"
     },
 
     current_user : {
-        url : `https://jana-s-backend-faty.vercel.app//api/user-details`,
+        url : `https://jana-s-backend-faty.vercel.app/api/user-details`,
         method : "get"
     },
 
     logout_user : {
-        url : `https://jana-s-backend-faty.vercel.app//api/userLogout`,
+        url : `https://jana-s-backend-faty.vercel.app/api/userLogout`,
         method : "get"
     },
 
     allUser : {
-        url : `https://jana-s-backend-faty.vercel.app//api/all-user`,
+        url : `https://jana-s-backend-faty.vercel.app/api/all-user`,
         method : "get"
     },
     updateUser : {
-        url : `https://jana-s-backend-faty.vercel.app//api/update-user`,
+        url : `https://jana-s-backend-faty.vercel.app/api/update-user`,
         method : "post"
     },
     searchUserInfo : {
-        url : `https://jana-s-backend-faty.vercel.app//api/search-user`,
+        url : `https://jana-s-backend-faty.vercel.app/api/search-user`,
         method : "post"
     },
     updatePassword : {
-        url : `https://jana-s-backend-faty.vercel.app//api/update-password`,
+        url : `https://jana-s-backend-faty.vercel.app/api/update-password`,
         method : 'put'
     },
     forgot_password : {
-        url : `https://jana-s-backend-faty.vercel.app//api/forgot-password`,
+        url : `https://jana-s-backend-faty.vercel.app/api/forgot-password`,
         method : 'put'
     },
     forgot_password_otp_verification : {
-        url : `https://jana-s-backend-faty.vercel.app//api/verify-forgot-password-otp`,
+        url : `https://jana-s-backend-faty.vercel.app/api/verify-forgot-password-otp`,
         method : 'put'
     },
     reset_password : {
-        url : `https://jana-s-backend-faty.vercel.app//api/reset-password`,
+        url : `https://jana-s-backend-faty.vercel.app/api/reset-password`,
         method : "put"
     },
 
     deleteUser : {
-        url : `https://jana-s-backend-faty.vercel.app//api/delete-user`,
+        url : `https://jana-s-backend-faty.vercel.app/api/delete-user`,
         method : 'delete'
     },
 
     getUserById : {
-        url : `https://jana-s-backend-faty.vercel.app//api/user/:userId`,
+        url : `https://jana-s-backend-faty.vercel.app/api/user/:userId`,
         method : "get"
     },
 
     uploadProduct : {
-        url : `https://jana-s-backend-faty.vercel.app//api/upload-product`,
+        url : `https://jana-s-backend-faty.vercel.app/api/upload-product`,
         method : "post"
     },
 
     allProduct : {
-        url : `https://jana-s-backend-faty.vercel.app//api/get-product`,
+        url : `https://jana-s-backend-faty.vercel.app/api/get-product`,
         method : 'get'
     },
 
     updateProduct : {
-        url : `https://jana-s-backend-faty.vercel.app//api/update-product`,
+        url : `https://jana-s-backend-faty.vercel.app/api/update-product`,
         method : 'post'
     },
 
     deleteProduct : {
-        url : `https://jana-s-backend-faty.vercel.app//api/delete-product/`,
+        url : `https://jana-s-backend-faty.vercel.app/api/delete-product/`,
         method : 'delete'
     },
 
     categoryProduct : {
-        url :  `https://jana-s-backend-faty.vercel.app//api/get-CategoryProduct`,
+        url :  `https://jana-s-backend-faty.vercel.app/api/get-CategoryProduct`,
         method : 'get'
     },
 
     categoryWiseProduct : {
-        url : `https://jana-s-backend-faty.vercel.app//api/category-product`,
+        url : `https://jana-s-backend-faty.vercel.app/api/category-product`,
         method : 'post'
     },
 
     productDetails : {
-        url : `https://jana-s-backend-faty.vercel.app//api/product-details`,
+        url : `https://jana-s-backend-faty.vercel.app/api/product-details`,
         method : 'post'
     },
 
     addToCartToProduct : {
-        url : `https://jana-s-backend-faty.vercel.app//api/addtocart`,
+        url : `https://jana-s-backend-faty.vercel.app/api/addtocart`,
         method : 'post'
     },
     addToCartProductCount : {
-        url : `https://jana-s-backend-faty.vercel.app//api/countAddToCartProduct`,
+        url : `https://jana-s-backend-faty.vercel.app/api/countAddToCartProduct`,
         method : 'get'
     },
     addToCartProductView : {
-        url : `https://jana-s-backend-faty.vercel.app//api/view-card-product`,
+        url : `https://jana-s-backend-faty.vercel.app/api/view-card-product`,
         method : 'get'
     },
 
     updateCartProduct : {
-        url : `https://jana-s-backend-faty.vercel.app//api/update-cart-product`,
+        url : `https://jana-s-backend-faty.vercel.app/api/update-cart-product`,
         method : 'post'
     },
 
     deleteCartProduct : {
-        url : `https://jana-s-backend-faty.vercel.app//api/delete-cart-product`,
+        url : `https://jana-s-backend-faty.vercel.app/api/delete-cart-product`,
         method : 'post'
     },
     searchProduct : {
-        url : `https://jana-s-backend-faty.vercel.app//api/search`,
+        url : `https://jana-s-backend-faty.vercel.app/api/search`,
         method : 'get'
     },
 
     filterProduct : {
-        url : `https://jana-s-backend-faty.vercel.app//api/filter-product`,
+        url : `https://jana-s-backend-faty.vercel.app/api/filter-product`,
         method : 'post'
     },
     payment : {
-        url : `https://jana-s-backend-faty.vercel.app//api/checkout`,
+        url : `https://jana-s-backend-faty.vercel.app/api/checkout`,
         method : 'post'
     },
     getOrder : {
-        url : `https://jana-s-backend-faty.vercel.app//api/order-list`,
+        url : `https://jana-s-backend-faty.vercel.app/api/order-list`,
         method : 'get'
     },
     allOrder : {
-        url : `https://jana-s-backend-faty.vercel.app//api/all-order`,
+        url : `https://jana-s-backend-faty.vercel.app/api/all-order`,
         method : 'get'
     },
 
     addAddress: {
-        url: `https://jana-s-backend-faty.vercel.app//api/add-address`,
+        url: `https://jana-s-backend-faty.vercel.app/api/add-address`,
         method: 'post'
     },
     allAddress: {
-        url: `https://jana-s-backend-faty.vercel.app//api/all-address`, // This fetches all addresses without needing a userId in the URL
+        url: `https://jana-s-backend-faty.vercel.app/api/all-address`, // This fetches all addresses without needing a userId in the URL
         method: 'get'
     },
     updateAddress: {
-        url: `https://jana-s-backend-faty.vercel.app//api/update-address`, // For updating a specific address
+        url: `https://jana-s-backend-faty.vercel.app/api/update-address`, // For updating a specific address
         method: 'put'
     },
     deleteAddress: {
-        url: `https://jana-s-backend-faty.vercel.app//api/delete-address`, // For deleting a specific address
+        url: `https://jana-s-backend-faty.vercel.app/api/delete-address`, // For deleting a specific address
         method: 'delete'
     },
 
     addReview : {
-        url : `https://jana-s-backend-faty.vercel.app//api/add-review`,
+        url : `https://jana-s-backend-faty.vercel.app/api/add-review`,
         method : 'post'
     },
 
     allReview : {
-        url : `https://jana-s-backend-faty.vercel.app//api/all-review`,
+        url : `https://jana-s-backend-faty.vercel.app/api/all-review`,
         method : 'get'
     },
     allReviewProductBased : {
-        url : `https://jana-s-backend-faty.vercel.app//api/:productId`,
+        url : `https://jana-s-backend-faty.vercel.app/api/:productId`,
         method : 'get'
     }
 
