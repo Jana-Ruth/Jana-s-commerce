5363438d401ed16f588cc21ef8fadd5466540b1f
Jana-Ruth <ruthewenode73@gmail.com>
2025-09-28 17:57:30 +0100

    removengn

--- src/ClientLayout/components/HorizontalCardProduct.js
+++ src/ClientLayout/components/HorizontalCardProduct.js
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect, useRef, useState } from "react";
 import fetchCategoryWiseProduct from "../../helpers/fetchCategoryWiseProduct";
 import displayUSDCurrency from "../../helpers/displayCurrency";
-import displayNGNCurrency from "../../helpers/displayNGNCurrency"; // Import NGN currency helper
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 import addToCart from "../../helpers/addToCart";
